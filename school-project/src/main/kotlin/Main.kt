a4be037983ef4988fd7bb4c1d3cfd495b9230a87
layinl <layon.final008@gmail.com>
2023-05-18 09:59:10 -0300

    Implementation finished, tested and documented

--- school-project/src/main/kotlin/Main.kt
+++ school-project/src/main/kotlin/Main.kt
@@ -1,20 +1,54 @@
+/**
+ * This enum represents different levels of difficulty of the content
+ */
 enum class Level { BASIC, INTERMEDIATE, HARD }
 
+/**
+ * The User class represents the classmate users. They can enroll in any [EducationalContent].
+ */
 class User(val name: String)
 
-data class EducationalContent(val name: String, var duration: Int = 60)
+/**
+ * The EducationalContent class represents the course that is given
+ */
+data class EducationalContent(val name: String, var duration: Int = 60, var level: Level)
 
+/**
+ * The Education class represents all the [EducationalContent] involved and contains all users enrolled
+ */
 data class Education(val name: String, var contents: List<EducationalContent>) {
 
   val subscribers = mutableListOf<User>()
 
+  /**
+   * Enrolls the user to the [Education]
+   * @param user the user to be enrolled
+   */
   fun enroll(user: User) {
-    // TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
     subscribers.add(user)
   }
+
+  /**
+   * Returns all the Education users' names
+   */
+  fun getClassmates(): List<String> {
+    return subscribers.map { user: User -> user.name }
+  }
+
 }
 
 fun main() {
-  // TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-  // TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+  val layin = User("layin")
+  val darkLayin = User("DarkLayin")
+  val aliny = User("Aliny")
+  val math = EducationalContent("Math", 30, Level.BASIC)
+  val physics = EducationalContent("Physics", level = Level.INTERMEDIATE)
+  var engineering = Education("Engineering", listOf(math, physics))
+
+  engineering.enroll(layin)
+  engineering.enroll(darkLayin)
+  engineering.enroll(aliny)
+
+  println(engineering.getClassmates())
+
 }
